3e2bab76d2a1e84eb01f516fe7a95f4ee8a21251
Nagmani Jha <nagmanijha31@gmail.com>
2025-10-16 00:15:34 +0530

    feat(ui): create basic Navbar and Footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
-import './App.css'
+import Navbar from './components/Navbar';
+import Footer from './components/common/Footer';
 
 function App() {
  
   return (
     <>
-
+      <Navbar />
+      <Footer />
     </>
   )
 }
